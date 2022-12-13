ec7c9104b43e1fe9065c414582d2072c405e529c
handro.dev <handro.dev@gmail.com>
2022-12-13 22:44:02 +0100

    Implemented solution for day 1, part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,27 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        // Keep track of maximum calories carried by any elf
+        var maxCalories = 0
+
+        // Keep track of calories carried by current elf
+        var calories = 0
+
+        for (line in input) {
+            if (line.isBlank()) {
+                // Empty line means another elf
+                if (calories > maxCalories)
+                    // Current elf is carrying more calories than anyone else
+                    // Update maximum
+                    maxCalories = calories
+                // Reset calories counter
+                calories = 0
+            } else {
+                // Still the same elf, keep counting calories carried
+                calories += line.toInt()
+            }
+        }
+
+        return maxCalories
     }
 
     fun part2(input: List<String>): Int {
@@ -8,10 +29,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // val testInput = readInput("Day01")
+    // check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+    // part2(input).println()
 }
